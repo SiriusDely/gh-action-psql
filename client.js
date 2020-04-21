62321f36cd15caf4eedad289a5167f6f2b4ad23f
Sirius Dely <siriusdely@icloud.com>
2020-04-21 17:55:42 +0700

    use connectionString to connect to postgres

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
+  /*
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   user: 'postgres',
   password: 'postgres',
   database: 'postgres'
+  */
+  connectionString: 'postgresql://postgres:postgres@posgres:5432/postgres'
 });
 
 pgclient.connect();
